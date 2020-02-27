46d7e032e313dc787eda3fbb62e175487cdb5d0b
Jacob Roush <jacobroushmail@gmail.com>
2020-02-27 13:02:26 -0600

    added code for push

--- index.js
+++ index.js
@@ -13,11 +13,30 @@ const rl = readline.createInterface({
 });
 
 server.on("connection", ws => {
+
+  const quotes = ['Do or do not, there is no try', 'If you\'re not first, you\'re last.', 'Hello there.'];
+
+  const quote = () => quotes[Math.floor(Math.random() * quotes.length)];
+
+  const id = setInterval(function () {
+    ws.send(`Quote: ${quote()}`, function () {
+      //
+      // Ignore errors.
+      //
+    });
+  }, 5000);
+
   ws.on("message", message => {
     console.log("received: %s", message);
+    ws.send(`echo ${message}`);
   });
 
-  ws.send("Running on localhost:8080. Send your message here:");
+  ws.on('close', function () {
+    console.log('stopping client interval');
+    clearInterval(id);
+  });
+
+  ws.send("Welcome to the WS server.");
 
   rl.on("line", input => {
     ws.send(input);
